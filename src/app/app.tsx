01d737b7d3f9629bdea98af811227cfa5fdbf6f2
cjgammon <gammon@adobe.com>
2021-02-05 14:52:19 -0600

    update subviews

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { QuizView } from "./views/quizView";
+import { QuizView } from "./views/quiz/quizView";
 
 export default class App extends React.Component<{}> {
 
